77c02b2690134dfa184063adaa7b9c993a39bd5b
RamSailopal <thelinuxguys@yahoo.com>
2022-04-05 15:42:15 +0100

    Adding a header to the file

--- yottadb/provision.sh
+++ yottadb/provision.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source `pkg-config --variable=prefix yottadb`/ydb_env_set
+echo -e "YottaDB Performance Statistics\n"
 echo "Database sequential set test"
 time ./dbtestset > results.txt
 echo -e "\nDatabase sequential get test"
